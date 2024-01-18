a5fc278f541eb0bbab85e796590a81055842dd10
mohitkumar00002 <153942274+mohitkumar00002@users.noreply.github.com>
2024-01-18 18:57:10 +0000

    basics

---
+++ basics/01_Variables.js
@@ -0,0 +1,13 @@
+const accountId = 20068;
+let accountEmail = "rajputmohit0002@gmail.com";
+var accountPass = 99887766;
+accountCity = "Jaipur";
+let accountName = "";
+
+
+
+// accountId = 10068; Not allowed
+accountEmail = "mohitkumar0002@gmail.com";
+accountPass = 11223344;
+accountCity = "Delhi"
+console.table([accountId, accountEmail, accountPass, accountCity])
---
+++ basics/02_Data_Types.js
@@ -0,0 +1,23 @@
+// There are 6 types of Data Types in Java Script
+
+// Shorthand - nn bb ss u
+
+/*
+Primitives Data Types -
+
+n - Number - 2 to power 53;
+n - null - standalone property;
+b - Boolean - true/false;
+b - BigInt - bigger value than the number limit;
+s - String - "Anything writtten inside the quote";
+s - Symbol - unique
+u - undefined - datatypes not defined
+
+
+object 
+ imp - type of null is object
+*/
+
+// find the data types of data
+let n = "name";
+console.log(typeof n)
