18c6744302d94ccda67126afe9b9dc9c8dba5902
mohitkumar00002 <153942274+mohitkumar00002@users.noreply.github.com>
2024-01-30 17:19:06 +0000

    conditional statements done

---
+++ main/08_Conditional_statements.js
@@ -0,0 +1,23 @@
+// let a = prompt("Hey whats your age?") 
+// a = Number.parseInt(a)
+// if(a<0){
+//   alert("This is an invalid age.")
+// }
+//   else if(a<9){
+//     alert("You ara a kid and you even cannot think of driving.")
+//   }
+//     else if(a<18 && a>=9){
+//       alert("You can now drive as you are above 18.")
+//     }
+// else{
+//   alert("This is a valid age and you are able to drive.")
+// }
+// console.log("Done")
+
+// //Ternary Operator
+// console.log("You can",a<18? "not drive":"drive")
+let age = 15
+console.log("You can",age<18? "not drive.":"drive.") 
+
+// switch case
+
---
+++ main/09_Switch_Case.js
@@ -0,0 +1,13 @@
+let month = "march"
+switch (month) {
+    case "jan": console.log("january");
+    break;
+    case "feb": console.log("february");
+    break;
+    case "march": console.log("march");
+    break;
+    case "april": console.log("april");
+    break;
+    default: console.log("Condition not match")
+    break;
+}
